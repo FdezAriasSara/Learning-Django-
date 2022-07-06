7f4b272ad2c4ceb52a3bf1c1c4cd1c647692248a
Sara Fernández Arias <uo269546@uniovi.es>
2022-07-06 10:18:08 +0200

    Implementation of PUT,GET & DELETE backend logic

--- Proyecto_API/api/urls.py
+++ Proyecto_API/api/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path 
 from .views import CompanyView
-urlpatterns=[path('companies/',CompanyView.as_view(),name='companies_list')]
+urlpatterns=[path('companies/',CompanyView.as_view(),name='companies_list'),
+             path('companies/<int:id>',CompanyView.as_view(),name='companies_process') 
+             #El path con el parámetro sirve para varias cosas (insertar una compañia , leer una compañía , actualizar... ) por eso tiene un endpoint más genérico.
+             #Su funcionalidad depende del método que se use en la petición.
+             ]
--- Proyecto_API/api/views.py
+++ Proyecto_API/api/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.views import View
 from .models import Company
+import json
 # Create your views here.
 #vista basada en una clase
 class CompanyView(View):
@@ -16,21 +17,49 @@ class CompanyView(View):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     #lista todas las compañías através del ORM(Object Relational Mapping)
-    def get(self, request):
-        companies=list(Company.objects.values())
-        #Es necesario convertirlo a list, para que sea un objeto conocido por python y por tanto , serializable a json
-        if len(companies)>0:
-           datos={'message':"Success", 'companies':companies}
+    def get(self, request,id=0):
+        if(id>0):
+            companies=list(Company.objects.filter(id=id).values())
+            if len(companies)>0:
+                company=companies[0]
+                datos={'message': "Success", 'company':company}
         else:
+          companies=list(Company.objects.values())
+         #Es necesario convertirlo a list, para que sea un objeto conocido por python y por tanto , serializable a json
+          if len(companies)>0:
+            datos={'message':"Success", 'companies':companies}
+          else:
            datos={'message':"Companies not found..."}
         return JsonResponse(datos)
         
     def post(self, request):
-        print(request.body)
+        #print(request.body)
+        jd=json.loads(request.body)#diccionario en el que se convierten los datos de json enviados
+        Company.objects.create(name=jd['name'],website=jd['website'],foundation=jd['foundation'])
         datos={'message':"Success"}
         return JsonResponse(datos)
         
-    def put(self,request):
-        pass
-    def delet(self,request):
-        pass
+    def put(self,request,id):
+        jd=json.loads(request.body)
+        companies=list(Company.objects.filter(id=id).values())
+        if len(companies) > 0:
+            #Aquí tenemos la certeza de que la compañía a actualizar, existe
+            company=Company.objects.get(id=id)
+            company.name=jd['name']
+            company.website=jd['website']
+            company.foundation=jd['foundation']
+            company.save()
+            datos={'message':"Success"}
+        else:
+            datos={'message':"Company not found..."}
+        return JsonResponse(datos)
+    def delete(self,request,id):
+        #Aquí no trabajamos con datos json
+        companies=list(Company.objects.filter(id=id).values())
+        if len(companies) > 0:
+            #El método delete está integrado en el ORM
+            Company.objects.filter(id=id).delete()
+            datos={'message':"Success"}
+        else:
+            datos={'message':"Company not found..."}
+        return JsonResponse(datos)
